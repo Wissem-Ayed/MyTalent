211dab9b4cfba3c48b6e0d4e0500eb93c46a212e
wis222 <wissemayed125@gmail.com>
2024-11-24 23:39:46 +0100

    initial commit

---
+++ src/main/java/com/example/MyTalent/MyTalentApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyTalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTalentApplication.class, args);
+	}
+
+}
